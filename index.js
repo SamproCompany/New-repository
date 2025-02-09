1be9202275381304999bd5742d49f5944cd5bfa0
SamproCompany <100447472+SamproCompany@users.noreply.github.com>
2025-02-09 21:58:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.onload = async function () {
   try {
     const response = await fetch('/config'); // Pobiera ENCRYPTION_KEY z backendu
     const data = await response.json();
-    document.getElementById('api-url').innerText = `ENCRYPTION KEY: ${data.encryptionKey}`;
+    document.getElementById('api-url').innerText = `ENCRIPTION_KEY: ${data.encriptionKey}`;
   } catch (error) {
     document.getElementById('api-url').innerText = "Error fetching ENCRYPTION_KEY.";
   }
