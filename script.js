0472e3a88f23fb09708a07b9bde0613dfe9a933c
SamproCompany <100447472+SamproCompany@users.noreply.github.com>
2025-02-09 20:07:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-import crypto from "https://cdnjs.cloudflare.com/ajax/libs/crypto-browserify/3.12.0/crypto-browserify.min.js";
-const key = Buffer.from(process.env.ENCRYPTION_KEY, "hex");
-console.log(key)
+fetch(".txt")
+    .then(response => response.text())
+    .then(data => {
+      console.log(data)
+    });
+
